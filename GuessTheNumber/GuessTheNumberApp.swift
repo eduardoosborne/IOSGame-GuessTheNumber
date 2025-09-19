c6a8e6c9c02bc3fc394d27e7b4f6b8f0096b41f5
Eduardo Osborne <eosborne@id.uff.br>
2025-09-19 09:22:10 -0300

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by Eduardo Osborne on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessTheNumberTests/GuessTheNumberTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberTests.swift
+//  GuessTheNumberTests
+//
+//  Created by Eduardo Osborne on 19/09/25.
+//
+
+import Testing
+@testable import GuessTheNumber
+
+struct GuessTheNumberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
